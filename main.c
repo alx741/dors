8b8ab510eeb1e4f4f6cd5164c46e38cbb2fa5011
Daniel Campoverde <alx@sillybytes.net>
2018-03-29 06:33:31 -0500

    Eyes bit matrix representation

--- main.c
+++ main.c
@@ -3,23 +3,49 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+void init_gpio(void);
+void shift_push(uint8_t c);
 void delay(void);
 
-void init_gpio(void)
+// Memory efficient 8x8 bit matrix
+typedef struct
 {
-    RCC_APB2ENR->IOPCEN = true;
-    PORTC->MODE13 = MODE_OUTPUT_2MHZ;
-    PORTC->CNF13 = CNF_OUT_PUSH_PULL;
-    PORTC->MODE14 = MODE_OUTPUT_2MHZ;
-    PORTC->CNF14 = CNF_OUT_PUSH_PULL;
-    PORTC->MODE15 = MODE_OUTPUT_2MHZ;
-    PORTC->CNF15 = CNF_OUT_PUSH_PULL;
-    PORTC->ODR13 = false;
-    PORTC->ODR14 = false;
-    PORTC->ODR15 = false;
+    unsigned row1_col1 : 1, row1_col2 : 1, row1_col3 : 1, row1_col4 : 1, row1_col5 : 1, row1_col6 : 1, row1_col7 : 1, row1_col8 : 1;
+    unsigned row2_col1 : 1, row2_col2 : 1, row2_col3 : 1, row2_col4 : 1, row2_col5 : 1, row2_col6 : 1, row2_col7 : 1, row2_col8 : 1;
+    unsigned row3_col1 : 1, row3_col2 : 1, row3_col3 : 1, row3_col4 : 1, row3_col5 : 1, row3_col6 : 1, row3_col7 : 1, row3_col8 : 1;
+    unsigned row4_col1 : 1, row4_col2 : 1, row4_col3 : 1, row4_col4 : 1, row4_col5 : 1, row4_col6 : 1, row4_col7 : 1, row4_col8 : 1;
+    unsigned row5_col1 : 1, row5_col2 : 1, row5_col3 : 1, row5_col4 : 1, row5_col5 : 1, row5_col6 : 1, row5_col7 : 1, row5_col8 : 1;
+    unsigned row6_col1 : 1, row6_col2 : 1, row6_col3 : 1, row6_col4 : 1, row6_col5 : 1, row6_col6 : 1, row6_col7 : 1, row6_col8 : 1;
+    unsigned row7_col1 : 1, row7_col2 : 1, row7_col3 : 1, row7_col4 : 1, row7_col5 : 1, row7_col6 : 1, row7_col7 : 1, row7_col8 : 1;
+    unsigned row8_col1 : 1, row8_col2 : 1, row8_col3 : 1, row8_col4 : 1, row8_col5 : 1, row8_col6 : 1, row8_col7 : 1, row8_col8 : 1;
+} EYE_t;
+
+
+const EYE_t some_eye =
+    { 0,0,0,0,0,0,0,0
+    , 0,0,0,0,0,0,0,0
+    , 0,0,1,0,0,0,0,0
+    , 0,0,0,1,0,0,0,0
+    , 0,0,0,0,0,0,0,0
+    , 0,0,0,0,0,0,0,0
+    , 0,0,0,0,0,0,0,0
+    , 0,0,0,0,0,0,0,0
+    };
+
+int main(void)
+{
+    rcc_setup_in_8mhz_hse_out_72mhz();
+    init_gpio();
+
+    shift_push(0b00000011);
+    shift_push(0b11111110);
+
+    while (true)
+    {
+    }
 }
 
-void shift_push(char c)
+void shift_push(uint8_t c)
 {
     PORTC->ODR13 = false;
     PORTC->ODR14 = false;
@@ -47,16 +73,18 @@ void shift_push(char c)
     PORTC->ODR14 = false;
 }
 
-int main(void)
+void init_gpio(void)
 {
-    rcc_setup_in_8mhz_hse_out_72mhz();
-    init_gpio();
-
-    shift_push(0b00001001);
-
-    while (true)
-    {
-    }
+    RCC_APB2ENR->IOPCEN = true;
+    PORTC->MODE13 = MODE_OUTPUT_2MHZ;
+    PORTC->CNF13 = CNF_OUT_PUSH_PULL;
+    PORTC->MODE14 = MODE_OUTPUT_2MHZ;
+    PORTC->CNF14 = CNF_OUT_PUSH_PULL;
+    PORTC->MODE15 = MODE_OUTPUT_2MHZ;
+    PORTC->CNF15 = CNF_OUT_PUSH_PULL;
+    PORTC->ODR13 = false;
+    PORTC->ODR14 = false;
+    PORTC->ODR15 = false;
 }
 
 void delay(void)
