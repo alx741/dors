dab5be7090eddf8687c565a2fd83bd37443baf83
Daniel Campoverde <alx@sillybytes.net>
2018-06-16 11:21:34 -0500

    PWM working on TIM3_CH1

--- firmware/main.c
+++ firmware/main.c
@@ -15,36 +15,37 @@ int main(void)
 
     // head control
     // SG90 - 50Hz carrier; 1ms - 2ms duty cycle
-    RCC_APB1ENR->TIM2EN = true;
+    RCC_APB1ENR->TIM3EN = true;
     RCC_APB2ENR->IOPAEN = true;
     RCC_APB2ENR->AFIOEN = true;
-    PORTA->MODE1 = MODE_OUTPUT_50MHZ;
-    PORTA->CNF1 = CNF_ALTERNATE_PUSH_PULL;
+    PORTA->MODE6 = MODE_OUTPUT_50MHZ;
+    PORTA->CNF6 = CNF_ALTERNATE_PUSH_PULL;
 
-	TIM2_CR1->CKD  = 0b00; // No clock division
-	TIM2_CR1->CMS  = 0b00; // Edge aligned mode
-	TIM2_CR1->DIR  = false; // upcounter
-	TIM2_CR1->OPM  = false; // continuous mode
+	TIM3_CR1->CKD  = 0b00; // No clock division
+	TIM3_CR1->CMS  = 0b00; // Edge aligned mode
+	TIM3_CR1->DIR  = false; // upcounter
+	TIM3_CR1->OPM  = false; // continuous mode
 
-    *TIM2_CNT  = 0;
-    *TIM2_PSC  = 1000;
-    *TIM2_ARR  = 10000;
-    *TIM2_CCR2 = 1000;
+    *TIM3_CNT  = 0;
+    *TIM3_PSC  = 1000;
+    *TIM3_ARR  = 10000;
+    *TIM3_CCR1 = 1000;
 
-    /* TIM2_CCMR1->CC2S = false; // Compare output */
-    /* TIM2_CCMR1->OC2PE = true; // Enable preload register */
-    /* TIM2_CCMR1->OC2M = 0b111; // PWM mode 2 */
-    static volatile uint16_t* const ccmr1 = (void*) _TIM2 + 0x18;
-    *ccmr1 = 0b0110100000000000;
+    /* TIM3_CCMR1->CC2S = false; // Compare output */
+    /* TIM3_CCMR1->OC2PE = true; // Enable preload register */
+    /* TIM3_CCMR1->OC2M = 0b111; // PWM mode 2 */
+    static volatile uint16_t* const ccmr1 = (void*) _TIM3 + 0x18;
+    /* *ccmr1 = 0b0110100000000000; */
+    *ccmr1 = 0b0000000001101000;
 
-    TIM2_CR1->ARPE = true; // ARR is buffered
+    TIM3_CR1->ARPE = true; // ARR is buffered
 
-    TIM2_EGR->UG = true; // Update generation
+    TIM3_EGR->UG = true; // Update generation
 
-    TIM2_CCER->CC2P = false; // Active high
-    TIM2_CCER->CC2E = true; // Signal is output
+    TIM3_CCER->CC1P = false; // Active high
+    TIM3_CCER->CC1E = true; // Signal is output
 
-    TIM2_CR1->CEN = true; // Counter enable
+    TIM3_CR1->CEN = true; // Counter enable
 
     while (true)
     {
