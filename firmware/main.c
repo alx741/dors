ae5261adc4b2a01a2ceefe182fb720a881df8cf0
Daniel Campoverde <alx@sillybytes.net>
2018-06-16 13:08:22 -0500

    Servo control working

--- firmware/main.c
+++ firmware/main.c
@@ -10,8 +10,8 @@ void delay(void);
 int main(void)
 {
     rcc_setup_in_8mhz_hse_out_72mhz();
-    /* eyes_init(); */
-    /* select_eyes(eye_happy, eye_happy); */
+    eyes_init();
+    select_eyes(eye_happy, eye_happy);
 
     // head control
     // SG90 - 50Hz carrier; 1ms - 2ms duty cycle
@@ -23,28 +23,23 @@ int main(void)
 
 	TIM3_CR1->CKD  = 0b00; // No clock division
 	TIM3_CR1->CMS  = 0b00; // Edge aligned mode
-	TIM3_CR1->DIR  = false; // upcounter
-	TIM3_CR1->OPM  = false; // continuous mode
-
-    *TIM3_CNT  = 0;
-    *TIM3_PSC  = 1000;
-    *TIM3_ARR  = 10000;
-    *TIM3_CCR1 = 1000;
-
-    /* TIM3_CCMR1->CC2S = false; // Compare output */
-    /* TIM3_CCMR1->OC2PE = true; // Enable preload register */
-    /* TIM3_CCMR1->OC2M = 0b111; // PWM mode 2 */
-    static volatile uint16_t* const ccmr1 = (void*) _TIM3 + 0x18;
-    /* *ccmr1 = 0b0110100000000000; */
-    *ccmr1 = 0b0000000001101000;
-
+	TIM3_CR1->DIR  = false; // Up counter
+	TIM3_CR1->OPM  = false; // Continuous mode
     TIM3_CR1->ARPE = true; // ARR is buffered
 
-    TIM3_EGR->UG = true; // Update generation
+    *TIM3_CNT  = 0;
+    *TIM3_PSC  = 3600; // 10kHz
+    *TIM3_ARR  = 200; // 50Hz
+    *TIM3_CCR1 = 20; // 10 - 50
 
-    TIM3_CCER->CC1P = false; // Active high
+    TIM3_CCMR1->CC1S = false; // Compare output
+    TIM3_CCMR1->OC1PE = true; // Enable preload register
+    TIM3_CCMR1->OC1M = 0b110; // PWM mode 1
+
+    TIM3_CCER->CC1P = false; // Active is high
     TIM3_CCER->CC1E = true; // Signal is output
 
+    TIM3_EGR->UG = true; // Generate update event
     TIM3_CR1->CEN = true; // Counter enable
 
     while (true)
