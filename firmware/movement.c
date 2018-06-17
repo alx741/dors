c8c70ec66f0afd8a4122f9d6092ad2cd9b4b3d59
Daniel Campoverde <alx@sillybytes.net>
2018-06-17 03:59:08 -0500

    New movement module

---
+++ firmware/movement.c
@@ -0,0 +1,35 @@
+#include "movement.h"
+#include <f1.h>
+#include <rcc.h>
+#include <stdbool.h>
+
+void movement_init()
+{
+    // SG90 - 50Hz carrier; 1ms - 2ms duty cycle
+    RCC_APB1ENR->TIM4EN = true;
+    RCC_APB2ENR->IOPBEN = true;
+    RCC_APB2ENR->AFIOEN = true;
+    PORTB->MODE6 = MODE_OUTPUT_50MHZ;
+    PORTB->CNF6 = CNF_ALTERNATE_PUSH_PULL;
+
+    TIM4_CR1->CKD  = 0b00; // No clock division
+    TIM4_CR1->CMS  = 0b00; // Edge aligned mode
+    TIM4_CR1->DIR  = false; // Up counter
+    TIM4_CR1->OPM  = false; // Continuous mode
+    TIM4_CR1->ARPE = true; // ARR is buffered
+
+    *TIM4_CNT  = 0;
+    *TIM4_PSC  = 3600; // 10kHz
+    *TIM4_ARR  = 200; // 50Hz
+    *TIM4_CCR1 = 33; // 10 - 50
+
+    TIM4_CCMR1->CC1S = false; // Compare output
+    TIM4_CCMR1->OC1PE = true; // Enable preload register
+    TIM4_CCMR1->OC1M = 0b110; // PWM mode 1
+
+    TIM4_CCER->CC1P = false; // Active is high
+    TIM4_CCER->CC1E = true; // Signal is output
+
+    TIM4_EGR->UG = true; // Generate update event
+    TIM4_CR1->CEN = true; // Counter enable
+}
---
+++ firmware/movement.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void movement_init(void);
