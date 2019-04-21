a712e745c3764e45e7950535618408f2b7418331
Daniel Campoverde <alx@sillybytes.net>
2019-04-20 20:02:29 -0500

    Define emotions as 8bit uints

--- firmware/emotion.h
+++ firmware/emotion.h
@@ -1,11 +1,12 @@
 #pragma once
+#include <stdint.h>
 
-typedef enum
-    { neutral
-    , happy
-    , sad
-    , surprised
-    , bored
-    } EMOTION_t;
-
+typedef uint8_t EMOTION_t;
 void convey_emotion(EMOTION_t e);
+
+// Emotions
+#define NEUTRAL   0x01
+#define HAPPY     0x02
+#define SAD       0x03
+#define SURPRISED 0x04
+#define BORED     0x05
