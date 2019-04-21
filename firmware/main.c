835487074ff24bd4047986ee5ab998739d593134
Daniel Campoverde <alx@sillybytes.net>
2019-04-20 21:19:57 -0500

    Use command loop

--- firmware/main.c
+++ firmware/main.c
@@ -1,22 +1,18 @@
+#include "eyes.h"
+#include "motion.h"
 #include <f1.h>
 #include <rcc.h>
 #include <usart.h>
 #include <stdbool.h>
 #include <stdint.h>
-
-/* #include <unistd.h> */
-
 #include <stdio.h>
-#include "eyes.h"
-#include "motion.h"
-#include "video.h"
 
 void delay(void);
 
 int main(void)
 {
     rcc_setup_in_8mhz_hse_out_72mhz();
-    usart_init_72mhz_9600baud();
+    command_init();
     eyes_init();
     motion_init();
 
@@ -25,61 +21,8 @@ int main(void)
     PORTC->CNF13 = CNF_OUT_PUSH_PULL;
     PORTC->ODR13 = true;
 
-    int c;
-    while (true)
-    {
-        printf("pitch: %d\tyaw: %d\n\n>  ", *TIM4_CCR1, *TIM4_CCR2);
-        c = getchar();
-
-        if ((char) c == 'j')
-        {
-            step_down();
-            PORTC->ODR13 ^= true;
-        }
-        else if ((char) c == 'k')
-        {
-            step_up();
-            PORTC->ODR13 ^= true;
-        }
-        else if ((char) c == 'h')
-        {
-            step_left();
-            PORTC->ODR13 ^= true;
-        }
-        else if ((char) c == 'l')
-        {
-            step_right();
-            PORTC->ODR13 ^= true;
-        }
-
-        else if ((char) c == 'H')
-        {
-            select_eyes(eye_happy, eye_happy);
-        }
-
-        else if ((char) c == 'n')
-        {
-            select_eyes(eye_normal, eye_normal);
-        }
-
-        else if ((char) c == 's')
-        {
-            select_eyes(eye_smile, eye_smile);
-        }
-
-        else if ((char) c == 'S')
-        {
-            select_eyes(eye_sad_up_left, eye_sad_up_right);
-        }
-
-        else if ((char) c == 'i')
-        {
-            PORTB->ODR5 ^= true;
-        }
-
-    }
+    command_loop();
 }
-
 
 void delay(void)
 {
