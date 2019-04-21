18861069bf7765efbc7cb84d2ce1e54e26e2d80c
Daniel Campoverde <alx@sillybytes.net>
2019-04-21 17:23:25 -0500

    Declare all emotions

--- firmware/emotion.h
+++ firmware/emotion.h
@@ -5,8 +5,13 @@ typedef uint8_t EMOTION_t;
 void convey_emotion(EMOTION_t e);
 
 // Emotions
-#define NEUTRAL   0x01
-#define HAPPY     0x02
-#define SAD       0x03
-#define SURPRISED 0x04
-#define BORED     0x05
+#define ANGRY      0x01
+#define BORED      0x02
+#define CONFUSED   0x03
+#define HAPPY      0x04
+#define NEUTRAL    0x05
+#define SAD        0x06
+#define SLEEPY     0x07
+#define SMILEY      0x08
+#define SURPRISED  0x09
+#define SUSPICIOUS 0x0A
