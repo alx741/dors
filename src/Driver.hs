53e674439b00f8b2551948b0027b7a76a9c73508
Daniel Campoverde <alx@sillybytes.net>
2019-04-22 09:07:19 -0500

    Register all emotions in driver

--- src/Driver.hs
+++ src/Driver.hs
@@ -33,11 +33,16 @@ data Direction
     deriving (Show)
 
 data Emotion
-    = Neutral
-    | Happy
-    | Sad
-    | Surprised
+    = Angry
     | Bored
+    | Confused
+    | Happy
+    | Neutral
+    | Sad
+    | Sleepy
+    | Smiley
+    | Surprised
+    | Suspicious
     deriving (Show)
 
 type RawCommand = Word8
@@ -69,11 +74,16 @@ instance RenderCommand Command where
     renderCommand (SetEmotion a) = 0x02 .|. shift (renderCommand a) 4
 
 instance RenderCommand Emotion where
-    renderCommand Neutral   = 0x01
-    renderCommand Happy     = 0x02
-    renderCommand Sad       = 0x03
-    renderCommand Surprised = 0x04
-    renderCommand Bored     = 0x05
+    renderCommand Angry      = 0x01
+    renderCommand Bored      = 0x02
+    renderCommand Confused   = 0x03
+    renderCommand Happy      = 0x04
+    renderCommand Neutral    = 0x05
+    renderCommand Sad        = 0x06
+    renderCommand Sleepy     = 0x07
+    renderCommand Smiley     = 0x08
+    renderCommand Surprised  = 0x09
+    renderCommand Suspicious = 0x0A
 
 instance RenderCommand Direction where
     renderCommand Up    = 0x01
