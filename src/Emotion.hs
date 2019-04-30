d14e04915fdbe89e39b6e3032a0e95d1d22ace67
Daniel Campoverde <alx@sillybytes.net>
2019-04-30 18:37:17 -0500

    Make lexicon a map from stems to emotions distributions

--- src/Emotion.hs
+++ src/Emotion.hs
@@ -4,15 +4,23 @@ import           Control.Monad                    (mzero)
 import qualified Data.ByteString.Char8            as BS
 import qualified Data.ByteString.Lazy             as LBS
 import           Data.Csv
+import           Data.HashMap.Strict              as HM
 import           Data.Text
 import           Data.Vector                      as V
 import           Numeric.Probability.Distribution as P
 import           Prelude                          hiding (Word)
 
-type Lexicon prob = Vector (Word prob)
+type Lexicon prob = HashMap Stem (P.T prob Emotion)
+type Stem = Text
+
+-- wordEmotionalDistribution :: Text -> P.T prob Emotion
+-- wordEmotionalDistribution = stem
 
 loadLexicon :: Fractional prob => FilePath -> IO (Lexicon prob)
-loadLexicon fp = LBS.readFile fp >>= pure . decode HasHeader >>= either error pure
+loadLexicon fp = decode HasHeader <$> LBS.readFile fp >>= either error (pure . vecToLexicon)
+    where
+         vecToLexicon :: Vector (Word prob) -> Lexicon prob
+         vecToLexicon = HM.fromList . fmap (\w -> (stem w, emotionDistribution w)) . V.toList
 
 data Emotion
     = Anticipation
@@ -29,7 +37,7 @@ data Emotion
     deriving (Show, Eq, Ord, Enum)
 
 data Word prob = Word
-    { stem                :: Text
+    { stem                :: !Text
     , emotionDistribution :: P.T prob Emotion
     } deriving (Show)
 
