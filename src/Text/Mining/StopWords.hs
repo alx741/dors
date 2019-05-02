6c21c8d52aa64b0b6b930dd34b13bbb24e54ced1
Daniel Campoverde <alx@sillybytes.net>
2019-05-02 17:38:41 -0500

    Stop words lexicon loading

--- src/Text/Mining/StopWords.hs
+++ src/Text/Mining/StopWords.hs
@@ -1,7 +1,22 @@
 module Text.Mining.StopWords where
 
-import Data.Text
+import           Data.Aeson
+import qualified Data.ByteString.Lazy as LBS
+import           Data.Maybe           (fromJust)
+import           Data.Set
+import           Data.Text
+
+import Text.Clean (removeAccents)
+
+type Lexicon = Set Text
 
 -- TODO: Implement
-removeStopWords :: Text -> Text
-removeStopWords = id
+removeStopWordsOnLexicon :: Lexicon -> Text -> Text
+removeStopWordsOnLexicon = undefined
+
+loadLexiconFile :: FilePath -> IO Lexicon
+loadLexiconFile fp
+    = fromList
+    . fmap removeAccents
+    . fromJust
+    . decode <$> LBS.readFile fp
