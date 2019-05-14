ab4a82cbdc9d738719f214738206070593b4b50b
Daniel Campoverde <alx@sillybytes.net>
2019-05-14 10:31:45 -0500

    Include noises in animations

--- src/Animation.hs
+++ src/Animation.hs
@@ -3,21 +3,27 @@ module Animation where
 import Control.Concurrent (threadDelay)
 
 import Driver
+import Voice  (Noise (..), makeNoise, say)
 
 -- | Wait n milliseconds
 wait :: Int -> IO ()
 wait = threadDelay . (\x -> x * 1000)
 
+noiseDir = "./data/sound"
+makeNoise' = makeNoise noiseDir
+
 wakeUpPhase1 :: IO ()
 wakeUpPhase1 = do
     setEyes Suspicious
     wait 3000
+    makeNoise' WakeupMumbleUp
     setHead (position 5) (position 5)
     wait 1000
     setEyes Bored
     wait 3000
     setEyes Suspicious
     wait 2000
+    makeNoise' WakeupMumbleDown
     setHead (position 0) (position 5)
     wait 1000
     robot Shutdown
@@ -26,14 +32,16 @@ wakeUpPhase2 :: IO ()
 wakeUpPhase2 = do
     setEyes Suspicious
     wait 3000
+    makeNoise' WakeupMumbleUp
     setHead (position 5) (position 5)
     wait 3000
     setEyes Suspicious
     wait 1000
     setEyes Angry
-    wait 1000
     setHead (position 5) (position 0)
+    makeNoise' Groan
     wait 2000
+    makeNoise' Groan
     setHead (position 5) (position 10)
     wait 2000
     setHead (position 5) (position 5)
@@ -50,6 +58,7 @@ sleep = do
     wait 3000
     setEyes Suspicious
     wait 3000
+    makeNoise' WakeupMumbleDown
     setHead (position 0) (position 5)
     wait 3000
     robot Shutdown
